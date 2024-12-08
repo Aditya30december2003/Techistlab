6bbdb9a3030dd63a92f03cf4a0232167217713e7
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-08 14:04:11 +0530

    about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,63 +1,71 @@
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="text-center py-[7rem]">
+      <h1 className="text-[3.4rem] font-bold">About Us</h1>
 
-      <div>
-        <h1>Our Mission</h1>
-        <p>We believe that technology should simplify life, not complicate it. Our mission is to help businesses harness the power of digital solutions to improve efficiency, reach new audiences, and deliver exceptional customer experiences.</p>
+      <div className="mt-10">
+        <h1 className="text-[2rem] font-bold">Our Mission</h1>
+        <p className="text-[1.1rem] mb-[3rem] w-[80%] mx-auto">We believe that technology should simplify life, not complicate it. Our mission is to help businesses harness the power of digital solutions to improve efficiency, reach new audiences, and deliver exceptional customer experiences.</p>
       </div>
 
-      <div>
-        <h1>Our Approach</h1>
+      <div><img className="lg:py-15 lg:px-20 p-4" src="https://images.ctfassets.net/kftzwdyauwt9/32cmTSUIF5POX5FMuoHJwO/be8b42b8016957ca28e07274f05f1d3d/stangel-2022-0527.webp?w=1920&q=90&fm=webp" alt="" /></div>
+
+      <div className="py-[3.7rem] px-[2rem]">
+        <h1 className="font-bold text-[2.4rem]">Our Approach</h1>
         
-        <div>
-          <div>
-          <h1>Discovery</h1>
+        <div className="flex flex-col lg:flex-row justify-between  items-center mt-[3rem] mx-auto shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-[1.3rem] w-full lg:w-[90%]">
+          <div className="text-left p-10 flex w-full lg:w-[40%] flex-col gap-5">
+          <h1 className="text-[2.2rem] font-bold">Discovery</h1>
           <p>We begin by understanding your goals, challenges, and target market, ensuring our strategies are aligned with your vision.</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="w-full lg:w-[50%]">
+            <img className="rounded-b-[1.3rem] lg:rounded-r-[1.3rem] w-full" src="https://images.ctfassets.net/kftzwdyauwt9/e632747f-9587-47a4-60779b6e0c90/cf75112eedea676e9deed512d191d1ac/planning-for-agi-and-beyond.jpg?w=750&q=90&fm=webp" alt="" />
           </div>
         </div>
 
-        <div>
-          <div>
-          <h1>Design</h1>
+        <div><img className="lg:py-15 lg:px-20 p-4 mt-24" src="https://images.ctfassets.net/kftzwdyauwt9/7fnM7WwfGlji0mE0t8DefO/cc161016b8d84fd6d789b06c92416544/stangel-openAI-offices-2022-1714.webp?w=1920&q=90&fm=webp" alt="" /></div>
+
+        <div className="flex flex-col lg:flex-row justify-between items-center mt-[3rem] mx-auto shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-[1.3rem] w-full lg:w-[90%]">
+          <div className="text-left p-10 flex w-full lg:w-[40%] flex-col gap-5">
+          <h1 className="text-[2.2rem] font-bold">Design</h1>
           <p>We focus on creating user-centric designs that prioritise intuition, aesthetics, and accessibility.</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="w-full lg:w-[50%]">
+            <img className="rounded-b-[1.3rem] lg:rounded-r-[1.3rem] w-full" src="https://images.ctfassets.net/kftzwdyauwt9/7JvDbOcLbpYuLKb5hK2IGa/175af8a9dac57d5862e03dc2da1ecd37/DALL_E_2024-04-05_07.40.png?w=750&q=90&fm=webp" alt="" />
           </div>
         </div>
 
-        <div>
-          <div>
-          <h1>Development</h1>
+        <div><img className="lg:py-15 lg:px-20 p-4 mt-24" src="https://images.ctfassets.net/kftzwdyauwt9/2ROfAZ7SlUiikj2Hp98LS0/7e71fd438017ce9cebe46fdce580bb04/stangel-2022-1534.webp?w=1920&q=90&fm=webp" alt="" /></div>
+
+        <div className="flex flex-col lg:flex-row justify-between items-center mt-[3rem] mx-auto shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-[1.3rem] w-full lg:w-[90%]">
+          <div  className="text-left p-10 flex  w-full lg:w-[40%] flex-col gap-5">
+          <h1 className="text-[2.2rem] font-bold">Development</h1>
           <p>Our development team ensures every product is secure, scalable, and optimised for performance.</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="w-full lg:w-[50%]">
+            <img className="rounded-b-[1.3rem] lg:rounded-r-[1.3rem] w-full" src="https://images.ctfassets.net/kftzwdyauwt9/dfdeca52-d054-4ce9-18d684209ff9/f3a2e6e71ebefb70fca386ff7bbb9d92/practices-for-governing-agentic-ai-systems.jpg?w=640&q=90&fm=webp" alt="" />
           </div>
         </div>
 
-        <div>
-          <div>
-          <h1>Testing & Launch</h1>
+
+
+        <div className="flex flex-col lg:flex-row justify-between items-center mt-[3rem] mx-auto shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-[1.3rem] w-full lg:w-[90%]">
+          <div className="text-left p-10 flex flex-col gap-5 w-full lg:w-[40%]">
+          <h1  className="text-[2.2rem] font-bold">Testing & Launch</h1>
           <p>Before going live, we rigorously test every aspect to ensure a flawless user experience.</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="w-full lg:w-[50%]">
+            <img className="rounded-b-[1.3rem] lg:rounded-r-[1.3rem] w-full" src="https://images.ctfassets.net/kftzwdyauwt9/5fR2bXCT8hpg6TzXqdcy8P/7414d82eb748d826db97eff7ccdf86d7/OAI_swe_bench_header.png?w=750&q=90&fm=webp" alt="" />
           </div>
         </div>
 
-        <div>
-          <div>
-          <h1>Continuous Improvement</h1>
+        <div className="flex flex-col lg:flex-row justify-between items-center mt-[3rem]  shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-[1.3rem] w-full lg:w-[90%] mx-auto">
+          <div className="text-left p-10 flex flex-col gap-5 w-[100%] lg:w-[40%]">
+          <h1 className="text-[2.2rem] font-bold">Continuous Improvement</h1>
           <p> We monitor performance, gather feedback, and iterate to help your product evolve with changing market needs.</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="w-[100%] lg:w-[50%]">
+            <img className="rounded-b-[1.3rem] lg:rounded-r-[1.3rem] w-full" src="https://images.ctfassets.net/kftzwdyauwt9/1UjP0G6IosB6ssRxjefGvU/4c86610cdaf7276f57a1363a53355f53/Fairness.png?w=750&q=90&fm=webp" alt="" />
           </div>
         </div>
 
