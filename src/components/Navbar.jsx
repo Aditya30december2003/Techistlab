c6ffe9b8c7acbb1a4ee8dcbdb201290392bc1f9a
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-08 12:03:40 +0530

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,96 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { GoSun } from "react-icons/go";
 import { FaMoon } from "react-icons/fa";
-import { useState } from 'react';
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
-  const [theme , setTheme] =useState(false)
-  const [nav , setNav]= useState(false);
+  const [theme, setTheme] = useState(false);
+  const [nav, setNav] = useState(false);
+
   return (
-    <>
     <div>
-    <div className='flex justify-between lg:justify-normal items-center px-2 py-4 mb-20 w-[100%] gap-5 border-b-2 border-black fixed bg-white top-0 left-0 z-50'>
-      <div className='text-[2rem] font-bold w-[20%] px-4'>Techistlab</div>
-      <div className='w-[80%] hidden lg:block'>
-        <ul className='flex items-center gap-5 text-[1rem] justify-between'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to='/careers'>Careers</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/portfolio'>Portfolio</Link>
-            <Link to='/services'>Services</Link>
-            <Link to='/team'>Teams</Link>
-            <div className='cursor-pointer border-2 p-2 border-black rounded-[100%]' onClick={()=>setTheme(!theme)}>{theme?<GoSun/>:<FaMoon/>}</div>
+      {/* Navbar */}
+      <div className="flex justify-between lg:justify-normal items-center px-2 py-4 mb-20 w-[100%] gap-5 border-b-2 border-black fixed bg-white top-0 left-0 z-50">
+        <div className="text-[2rem] font-bold w-[20%] px-4">Techistlab</div>
+        <div className="w-[80%] hidden lg:block">
+          <ul className="flex items-center gap-5 text-[1rem] justify-between">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/careers">Careers</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/team">Teams</Link>
+            <div
+              className="cursor-pointer border-2 p-2 border-black rounded-[100%]"
+              onClick={() => setTheme(!theme)}
+            >
+              {theme ? <GoSun /> : <FaMoon />}
+            </div>
+          </ul>
+        </div>
+        {/* Hamburger Icon */}
+        <div
+          className="block lg:hidden"
+          onClick={() => setNav(!nav)}
+        >
+          {!nav?<GiHamburgerMenu size={30} className="cursor-pointer" />:<RxCross1 size={30} />}
+        </div>
+      </div>
+
+      {/* Mobile Navigation */}
+      <div
+        className={`fixed top-0 left-0 w-full h-screen bg-white text-black-400 transform ${
+          nav ? "translate-y-0" : "-translate-y-full"
+        } transition-transform duration-500 z-10 lg:hidden`}
+      >
+        {/* Close Icon */}
+        <div
+          onClick={() => setNav(false)}
+          className="ml-[85%] mt-5 bg-gray-500 p-2 rounded-[100%] cursor-pointer"
+        >
+         
+        </div>
+        {/* Mobile Menu Links */}
+        <ul className="flex flex-col items-center gap-5 text-[1rem] justify-between mt-10 py-5">
+          <Link to="/" onClick={() => setNav(false)}>
+            Home
+          </Link>
+          <Link to="/about" onClick={() => setNav(false)}>
+            About
+          </Link>
+          <Link to="/blog" onClick={() => setNav(false)}>
+            Blog
+          </Link>
+          <Link to="/careers" onClick={() => setNav(false)}>
+            Careers
+          </Link>
+          <Link to="/contact" onClick={() => setNav(false)}>
+            Contact
+          </Link>
+          <Link to="/portfolio" onClick={() => setNav(false)}>
+            Portfolio
+          </Link>
+          <Link to="/services" onClick={() => setNav(false)}>
+            Services
+          </Link>
+          <Link to="/team" onClick={() => setNav(false)}>
+            Teams
+          </Link>
+          <div
+            className="cursor-pointer border-2 p-2 border-white rounded-[100%]"
+            onClick={() => setTheme(!theme)}
+          >
+            {theme ? <GoSun /> : <FaMoon />}
+          </div>
         </ul>
       </div>
-      <div className='block lg:hidden'><GiHamburgerMenu size={30} className='cursor-pointer'/></div>
     </div>
-    
-    <div className={!nav?'top-0 left-0 bg-blue-900 w-full text-white lg:hidden h-screen mt-[-250%] duration-1000 fixed z-10':'top-0 left-0 z-10 fixed w-full duration-1000 bg-blue-900 overflow-hidden text-white lg:hidden h-screen mt-[0%]'}>
-    <div onClick={()=>{setNav(!nav)}} className='ml-[85%] mt-5 bg-gray-500 p-2 rounded-[100%]'><RxCross1 size={35} className='z-20 duration-300 cursor-pointer' /></div>
-    <ul className='flex flex-col items-center gap-5 text-[1rem] justify-between'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to='/careers'>Careers</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/portfolio'>Portfolio</Link>
-            <Link to='/services'>Services</Link>
-            <Link to='/team'>Teams</Link>
-            <div className='cursor-pointer border-2 p-2 border-black rounded-[100%]' onClick={()=>setTheme(!theme)}>{theme?<GoSun/>:<FaMoon/>}</div>
-      </ul>
-    </div>
-
-    </div>
-    </>
-  )
-}
+  );
+};
 
 export default Navbar;
 
