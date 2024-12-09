bca652a02649ca5e64fd1ccaa5dfe7ceadaf33d7
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-09 09:43:11 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaGlobe } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='w-full mt-20 bottom-0 bg-gray-300 text-black p-20 '>
+    <div className="flex flex-col lg:flex-row gap-4">
+      <div className='flex flex-col gap-3'>
+        <h1 className='font-bold text-[1.1rem]'>Techistlab</h1>
+        <Link className='text-[0.85rem] font-light'>About</Link>
+        <Link className='text-[0.85rem] font-light'>Blog</Link>
+        <Link className='text-[0.85rem] font-light'>Careers</Link>
+        <Link className='text-[0.85rem] font-light'>Contact</Link>
+        <Link className='text-[0.85rem] font-light'>Portfolio</Link>
+        <Link className='text-[0.85rem] font-light'>Services</Link>
+        <Link className='text-[0.85rem] font-light'>Teams</Link>
+      </div>
+      <div className='flex flex-col gap-3'>
+        <h1 className='font-bold text-[1.1rem]'>Careers</h1>
+        <Link  className='text-[0.85rem] font-light'>Job Search</Link>
+        <Link></Link>
+      </div>
     </div>
-  )
-}
+    <div className='w-[80%] mt-10 bg-black h-[0.03rem]'></div>
+    <div className='flex flex-col lg:flex-row items-center gap-7 lg:gap-3 text-[0.85rem] mt-5 justify-between'>
+      <div className='flex items-center gap-3'>
+      <p>© 2024 Techistlab</p>
+      <p>.</p>
+      <p>Privacy</p>
+      <p>.</p>
+      <p>Terms</p>
+      </div>
+      <div className='flex flex-row  items-center gap-3'>
+        <p><FaGlobe size={20}/></p>
+        <p><FaFacebookSquare size={20}/></p>
+        <p><FaSquareXTwitter size={20}/></p>
+        <p><FaInstagramSquare size={20}/></p>
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
+
