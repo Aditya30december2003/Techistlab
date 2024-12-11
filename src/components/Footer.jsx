d948eb7f3e4cce3ba1b8b6c895b7698d91798452
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-12 02:48:36 +0530

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,62 +1,9 @@
-import {useState , useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { FaGlobe } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagramSquare } from "react-icons/fa";
-import PageLoadAnimation from './PageLoadAnimation';
-
-const fetchHomeContent = async () => {
-  try {
-    const response = await databases.listDocuments(
-      "67594afc0000cafabf62", // Your database ID
-      "67594b2b0006d71a548d"  // Your "home" collection ID
-    );
-    
-    console.log("Raw Appwrite Response:", response);
-    console.log("Documents:", response.documents);
-    console.log("Total documents:", response.documents.length);
-    
-    if (response.documents.length > 0) {
-      console.log("First document full details:", response.documents[0]);
-      console.log("taglineA value:", response.documents[0].taglineA);
-    }
-    
-    return response.documents;
-  } catch (error) {
-    console.error("Detailed Appwrite Fetch Error:", {
-      message: error.message,
-      code: error.code,
-      type: error.type
-    });
-    throw error;
-  }
-};
 const Footer = () => {
-  const [content, setContent] = useState(null);
-  const [error, setError] = useState(null);
-  useEffect(()=>{
-    const loadContent = async () => {
-      try {
-        const data = await fetchHomeContent();
-        console.log("Fetched data in component:", data);
-        
-        if (data && data.length > 0) {
-          console.log("Setting content:", data[0]);
-          setContent(data[0]);
-        } else {
-          console.warn("No documents found");
-          setError("No content available");
-        }
-      } catch (err) {
-        console.error("Component fetch error:", err);
-        setError(err.message);
-      }
-    };
-
-    loadContent();
-  },[])
-  
   return (
     <div className="w-full bg-gray-300 text-black p-20">
       <div className="flex flex-col lg:flex-row gap-10">
