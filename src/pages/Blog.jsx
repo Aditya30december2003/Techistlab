f221c2f4be0d82d1d56b93dd3c9adfacb719bb0a
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-11 09:13:16 +0530

    add blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -22,19 +22,19 @@ const Blog = () => {
         <div className="flex flex-col lg:grid lg:grid-cols-2 gap-20 p-5 text-white mt-20">
           <div className="cursor-pointer rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] hover:rounded-md duration-700">
             <div className="absolute h-[20rem]">
-              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto">The Future of Mobile Commerce: Trends to Watch in 2024</h1>
+              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto bg-purple-300 w-[80%] rounded-md">The Future of Mobile Commerce: Trends to Watch in 2024</h1>
             </div>
             <div><img className="h-[20rem] w-full object-cover rounded-md" src="https://images.pexels.com/photos/5076525/pexels-photo-5076525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /></div>
           </div>
           <div className="cursor-pointer rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] hover:rounded-md duration-700">
             <div className="absolute">
-              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto">User-Centric Design: Why It Matters More Than Ever</h1>
+              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto bg-purple-300 w-[80%] rounded-md">User-Centric Design: Why It Matters More Than Ever</h1>
             </div>
             <div><img className="h-[20rem] w-full object-cover rounded-md" src="https://images.pexels.com/photos/221185/pexels-photo-221185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /></div>
           </div>
           <div className="cursor-pointer rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-[5px_5px_0px_0px_rgba(109,40,217)] hover:rounded-md duration-700">
             <div className="absolute">
-              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto">Developing for Scalability: Best Practices in Software Engineering</h1>
+              <h1 className="text-xl text-center font-bold mt-20 p-10 mx-auto bg-purple-300 w-[80%] rounded-md">Developing for Scalability: Best Practices in Software Engineering</h1>
             </div>
             <div><img className="h-[20rem] w-full object-cover rounded-md" src="https://images.pexels.com/photos/8127695/pexels-photo-8127695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /></div>
           </div>
