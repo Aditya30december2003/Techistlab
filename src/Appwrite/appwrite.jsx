3550c826404ee5f78a7a61e49cfe5220cd07fd72
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-12 02:13:46 +0530

    changes reversed

--- src/Appwrite/appwrite.jsx
+++ src/Appwrite/appwrite.jsx
@@ -1,15 +1,11 @@
 import { Client, Databases } from 'appwrite';
-
-// Initialize the Appwrite client
 const client = new Client();
+
 client
   .setEndpoint('https://centralapps.hivefinty.com/v1') // Use Appwrite Cloud endpoint
   .setProject('67594a9e0005829e0c75'); // Replace with your project ID
 
-// Initialize the Databases instance
+
 const databases = new Databases(client);
 
-// Example: fetching documents from a collection
-
 export { client, databases };
-
