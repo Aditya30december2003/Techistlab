c3b13b69ae3890e693214477b97d3636bd33fb58
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-12 02:24:44 +0530

    made changes

--- src/Appwrite/appwrite.jsx
+++ src/Appwrite/appwrite.jsx
@@ -1,11 +1,14 @@
 import { Client, Databases } from 'appwrite';
+
+// Initialize the Appwrite client
 const client = new Client();
 
 client
   .setEndpoint('https://centralapps.hivefinty.com/v1') // Use Appwrite Cloud endpoint
   .setProject('67594a9e0005829e0c75'); // Replace with your project ID
 
-
+// Initialize the Databases service
 const databases = new Databases(client);
 
 export { client, databases };
+
