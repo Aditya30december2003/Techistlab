2a6859f4432c45a422d722b9d3504b44ddce7d3a
Aditya30december2003 <adityasmjain@gmail.com>
2024-12-13 17:38:38 +0530

    changes made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { FaInstagramSquare } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="w-full bg-gray-300 text-black p-4 lg:p-20">
-      <div className='mb-32'>
+      <div className='mb-0'>
       <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -40,13 +40,13 @@ const Footer = () => {
       </div>
       <div className="flex flex-col lg:flex-row gap-[2rem] lg:gap-[14rem] items-center">
   <div className="flex justify-center items-center h-screen">
-  <div className="bg-gray-400 rounded-full w-[21rem] h-[21rem] lg:w-[24rem] lg:h-[24rem] flex-col text-center justify-center items-center">
+  <div className="bg-gray-400 rounded-full w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] flex-col text-center justify-center items-center">
     <img 
       src="https://techistlab.co.uk/assets/logo-D8JJhk6e.png" 
       alt="Logo" 
       className="w-48 h-48 object-contain mx-auto"
     />
-    <div className='font-bold text-[1rem] lg:text-[1.4rem]'>
+    <div className='font-bold text-[1rem] lg:text-[1.2rem]'>
   167-169 Great Portland Street, 5th Floor, London, England, W1W 5PF
   +44(0)20 3723 6703
   </div>
@@ -55,13 +55,14 @@ const Footer = () => {
 
         <div className="flex flex-col gap-3">
           <h1 className="font-bold text-[2.3rem]">Techistlab</h1>
-          <Link to="/about"  className="text-[1.25rem] font-light">About</Link>
-          <Link to="/blog"  className="text-[1.25rem] font-light">Blog</Link>
-          <Link to="/careers"  className="text-[1.25rem] font-light">Careers</Link>
-          <Link to="/contact"  className="text-[1.25rem] font-light">Contact</Link>
-          <Link to="/portfolio"  className="text-[1.25rem] font-light">Portfolio</Link>
-          <Link to='services' className="text-[1.25rem] font-light">Services</Link>
-          <Link to='teams' className="text-[1.25rem] font-light">Teams</Link>
+          <Link to="/about"  className="text-[1.45rem] font-light">About</Link>
+          <Link to="/blog"  className="text-[1.45rem] font-light">Blog</Link>
+          <Link to="/careers"  className="text-[1.45rem] font-light">Careers</Link>
+          <Link to="/contact"  className="text-[1.45rem] font-light">Contact</Link>
+          <Link to="/portfolio"  className="text-[1.45rem] font-light">Portfolio</Link>
+          <Link to='services' className="text-[1.45rem] font-light">Services</Link>
+          <Link to='teams' className="text-[1.45rem] font-light">Teams</Link>
+          <Link className="text-[1.45rem] font-light">Legal</Link>
         </div>
         <div className="flex flex-col gap-3 text-[2.3rem]">
           <h1 className="font-bold text-[2.3rem]">Careers</h1>
